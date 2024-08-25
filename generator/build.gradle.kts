1ad96733e5017844aa6108a251096d16f742c8a0
Karl-Andero Mere <anddero@gmail.com>
2024-08-25 21:59:45 +0300

    Fix warning about kotlin gradle plugin loaded multiple times.

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm")
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
     id("org.jetbrains.compose") version "1.6.11"
 }
